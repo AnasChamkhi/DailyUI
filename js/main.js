7b39ee675ec679825aaa252ecfbbbef7a360a2bf
Donguapo <chamkhianas@gmail.com>
2019-12-31 12:20:11 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var modal = document.getElementById("myModal");
+var close = document.getElementById("close");
+var ModalS=document.getElementById("modalSearch")
+
+
+
+
+
+function mouseOver(){
+
+
+        document.getElementById("first").classList.remove("active");
+
+
+}   
+
+function mouseOut(){
+
+
+    document.getElementById("first").classList.add("active");
+
+
+}
+
+
+function Modal(){
+
+    modal.style.display = "block";
+
+}
+
+
+
+
+
+function Close(){
+
+
+    modal.style.display = "none";
+
+
+}
+
+function ModalSearch(){
+
+
+    ModalS.style.display = "block";
+
+}
+
+function CloseS(){
+
+
+    ModalS.style.display = "none";
+
+
+}
+
+
+
+
+
+console.log("☭ Made With Love At @Youcode By Anas Ch@mkhi ☭")
+console.warn("☭ Made With Love At @Youcode By Anas Ch@mkhi ☭")
